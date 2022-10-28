b056d0972baa2ae9dfe0a91b68a3f5238b4e372c
DanielDarwiche <danielinti1@hotmail.com>
2022-10-28 14:50:42 +0200

    almost finished...

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security;
+using System.Threading;
 
 namespace HangMan
 {
@@ -7,56 +10,314 @@ namespace HangMan
     {
         static void Main(string[] args)
         {
-
-            string orr= Word();  //ORDET SOM HÄMTATS MEN INTE VISATS ÄN! SOM SKA ANVÄDAS
-            //Console.WriteLine($"--~~== {orr} ==~~--");
-
-
-
-            ///visa ordet i *** dolt, om inskrivet tecken == samma som i gissningsord VISA tecken 
-            //rita gubbe metod,   fel variabel , om fel=1 rita gubbe +1
-            SecureString passworrrdd =maskInputString();
-            string Password = new System.Net.NetworkCredential(string.Empty, passworrrdd).Password;
+            Game();
+        }
+        public static void Game()
+        {
+            string ordet = "ha";                                                                     //Word();  
+            List<char> exempelord = new List<char>();  //creating list for exampleword
+            foreach (char c in ordet)//loop putting all chars of word in exampleword
+            {
+                exempelord.Add(c);
+            }
+            char gissningsbokstav = ' '; //letterguess
+            List<char> rättcharlist = new List<char>();//list for right guesses
+            List<char> gissningar = new List<char>();//list for all guesses
+            bool won = false;
+            Console.Clear();
+            Console.WriteLine("Hela ordet du ska gissa på är:");//shows amount of letters in the word before playing
+            foreach (int e in ordet)
+            {
+                Console.Write("*");
+            }
             Console.WriteLine();
-            Console.WriteLine(Password);
-            Console.ReadKey();
+            int point = ordet.Length;
+            int fel = 0;
+            do
+            {
+                Console.WriteLine("Skriv in en bokstav:");
+                gissningsbokstav = Console.ReadKey().KeyChar; //enter a char into gissningsbokstav
+                Console.WriteLine();
+                gissningar.Add(gissningsbokstav);           //add letter to allguesses
+                for (int i = 0; i < exempelord.Count; i++)
+                {
+                    if (rättcharlist.Contains(exempelord[i]))
+                    {
+                        Console.Write(exempelord[i]);//write out letter
+                    }
+                    else if (exempelord[i] == gissningsbokstav)//if guessingletter == letter in word
+                    {
+                        Console.Write(gissningsbokstav);            //write letter
+                        rättcharlist.Add(gissningsbokstav);         //add letter to rightguess
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                    if (rättcharlist.Count == point)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("\nDu vann!");//You won 
+                        Console.WriteLine($" --~~== {ordet} ==~~--");
+                        Thread.Sleep(5000);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Game();
+                    }
+                }
+                Console.WriteLine("\n");       //new line     
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Du har gissat fel på: ");
+                foreach (char giss in gissningar)
+                {
+                    if (!rättcharlist.Contains(giss))
+                    {
+                        Console.Write("{0} ", giss);
+                    }
+                    else { }
+                }
+             
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if (fel > 0)
+                {
+                    Man(fel);
+                }
+                Console.WriteLine("\n_____________________");       //new line     
+            } while (won == false);
+
         }
         public static string Word()
         {
             //List <string> ordgenerator  = new List<string>()
             String[] ordgenerator = new string[]
-            { "onomatopeia", "flodhäst","automatisera","kärleksfull","mjukvaruutvecklare",
-                "akrobatisk", "kampsportsutövare", "långhårig","näringsidkare","filosofilektör", "innehållslista"};//11 ord
-            //for loop   if index = randooo Console Write ordgenrator[i]
+            { "onomatopeia", "flodhäst","automatisera","kärleksfull","mjukvaruutvecklare","psykopatisk","simbassäng", "myntsamlare","receptnedskrivare","motorvägsanläggare", "hängagubbespelsutvecklare",
+                "akrobatisk", "kampsportsutövare", "långhårig","näringsidkare","filosofilektör", "innehållslista"};//17 ord
             Random display = new Random();
-            int randoooo = display.Next(11); //random siffra skapas i randoooo
+            int randoooo = display.Next(17); //random siffra skapas i randoooo
             string GissningsOrd = ordgenerator[randoooo];
-            //Console.WriteLine($"--~~== {GissningsOrd} ==~~--");
             return GissningsOrd;
             ////SKAPAR GISSNINGSORD
         }
-        private static SecureString maskInputString()
+        public static void Man(int fel)
         {
-            Console.WriteLine("Enter your password");
-            SecureString pass = new SecureString();
-            ConsoleKeyInfo keyInfo;
+            int höjd = 20;
+            int bredd = 20;
+            string[,] gubbe = new string[höjd, bredd];
+            switch (fel)
+            {
+                case 1:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    break;
+                case 2:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    //stolpen  2
+                    gubbe[11, 17] = "|";
+                    gubbe[12, 17] = "|";
+                    gubbe[13, 17] = "|";
+                    gubbe[14, 17] = "|";
+                    gubbe[15, 17] = "|";
+                    gubbe[16, 17] = "|";
+                    break;
+                case 3:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    //stolpen  2
+                    gubbe[11, 17] = "|";
+                    gubbe[12, 17] = "|";
+                    gubbe[13, 17] = "|";
+                    gubbe[14, 17] = "|";
+                    gubbe[15, 17] = "|";
+                    gubbe[16, 17] = "|";
+                    // plankan   3
+                    gubbe[5, 8] = "¤";
+                    gubbe[5, 9] = "¤";
+                    gubbe[5, 10] = "¤";
+                    gubbe[5, 11] = "¤";
+                    gubbe[5, 12] = "¤";
+                    gubbe[5, 13] = "¤";
+                    gubbe[5, 14] = "¤";
+                    gubbe[5, 15] = "¤";
+                    gubbe[5, 16] = "¤";
+                    gubbe[5, 17] = "|";
+                    gubbe[6, 17] = "|";
+                    gubbe[7, 17] = "|";
+                    gubbe[8, 17] = "|";
+                    gubbe[9, 17] = "|";
+                    gubbe[10, 17] = "|";
+                    break;
+                case 4:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    //stolpen  2
+                    gubbe[11, 17] = "|";
+                    gubbe[12, 17] = "|";
+                    gubbe[13, 17] = "|";
+                    gubbe[14, 17] = "|";
+                    gubbe[15, 17] = "|";
+                    gubbe[16, 17] = "|";
+                    // plankan   3
+                    gubbe[5, 8] = "¤";
+                    gubbe[5, 9] = "¤";
+                    gubbe[5, 10] = "¤";
+                    gubbe[5, 11] = "¤";
+                    gubbe[5, 12] = "¤";
+                    gubbe[5, 13] = "¤";
+                    gubbe[5, 14] = "¤";
+                    gubbe[5, 15] = "¤";
+                    gubbe[5, 16] = "¤";
+                    //repet   4
+                    gubbe[5, 17] = "|";
+                    gubbe[6, 17] = "|";
+                    gubbe[7, 17] = "|";
+                    gubbe[8, 17] = "|";
+                    gubbe[9, 17] = "|";
+                    gubbe[10, 17] = "|";
+                    gubbe[5, 7] = "|";
+                    gubbe[6, 7] = "|";
+                    gubbe[7, 7] = "|";
+                    break;
+                case 5:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    //stolpen  2
+                    gubbe[11, 17] = "|";
+                    gubbe[12, 17] = "|";
+                    gubbe[13, 17] = "|";
+                    gubbe[14, 17] = "|";
+                    gubbe[15, 17] = "|";
+                    gubbe[16, 17] = "|";
+                    // plankan   3
+                    gubbe[5, 8] = "¤";
+                    gubbe[5, 9] = "¤";
+                    gubbe[5, 10] = "¤";
+                    gubbe[5, 11] = "¤";
+                    gubbe[5, 12] = "¤";
+                    gubbe[5, 13] = "¤";
+                    gubbe[5, 14] = "¤";
+                    gubbe[5, 15] = "¤";
+                    gubbe[5, 16] = "¤";
+                    //repet   4
+                    gubbe[5, 17] = "|";
+                    gubbe[6, 17] = "|";
+                    gubbe[7, 17] = "|";
+                    gubbe[8, 17] = "|";
+                    gubbe[9, 17] = "|";
+                    gubbe[10, 17] = "|";
+                    gubbe[5, 7] = "|";
+                    gubbe[6, 7] = "|";
+                    gubbe[7, 7] = "|";
 
-            do
+                    gubbe[8, 7] = "Q";
+                    gubbe[9, 6] = "/";
+                    gubbe[9, 8] = @"\";
+                    gubbe[9, 7] = "|";
+                    gubbe[10, 7] = "|";
+                    break;
+                case 6:
+                    //Botten  1
+                    gubbe[17, 17] = "^";
+                    gubbe[18, 16] = "/";
+                    gubbe[18, 18] = @"\";
+                    gubbe[19, 15] = "/";
+                    gubbe[19, 19] = @"\";
+                    gubbe[18, 17] = "|";
+                    gubbe[19, 17] = "|";
+                    //stolpen  2
+                    gubbe[11, 17] = "|";
+                    gubbe[12, 17] = "|";
+                    gubbe[13, 17] = "|";
+                    gubbe[14, 17] = "|";
+                    gubbe[15, 17] = "|";
+                    gubbe[16, 17] = "|";
+                    // plankan   3
+                    gubbe[5, 8] = "¤";
+                    gubbe[5, 9] = "¤";
+                    gubbe[5, 10] = "¤";
+                    gubbe[5, 11] = "¤";
+                    gubbe[5, 12] = "¤";
+                    gubbe[5, 13] = "¤";
+                    gubbe[5, 14] = "¤";
+                    gubbe[5, 15] = "¤";
+                    gubbe[5, 16] = "¤";
+                    //repet   4
+                    gubbe[5, 17] = "|";
+                    gubbe[6, 17] = "|";
+                    gubbe[7, 17] = "|";
+                    gubbe[8, 17] = "|";
+                    gubbe[9, 17] = "|";
+                    gubbe[10, 17] = "|";
+                    gubbe[5, 7] = "|";
+                    gubbe[6, 7] = "|";
+                    gubbe[7, 7] = "|";
+
+                    gubbe[8, 7] = "Q";
+                    gubbe[9, 6] = "/";
+                    gubbe[9, 8] = @"\";
+                    gubbe[9, 7] = "|";
+                    gubbe[10, 7] = "|";
+                    gubbe[11, 7] = "^";
+                    gubbe[12, 6] = "/";
+                    gubbe[12, 8] = @"\";
+                    //GAME OVER!  6
+                    gubbe[13, 3] = "G";
+                    gubbe[13, 4] = "A";
+                    gubbe[13, 5] = "M";
+                    gubbe[13, 6] = "E";
+                    gubbe[13, 7] = " ";
+                    gubbe[13, 8] = "O";
+                    gubbe[13, 9] = "V";
+                    gubbe[13, 10] = "E";
+                    gubbe[13, 11] = "R";
+                    Thread.Sleep(10000);
+                    Environment.Exit(0);
+                    break;
+            }
+            for (int i = 0; i < höjd; i++)
             {
-                keyInfo = Console.ReadKey(true);
-                if (!char.IsControl(keyInfo.KeyChar))
+                for (int h = 0; h < bredd; h++)
                 {
-                    pass.AppendChar(keyInfo.KeyChar);
-                    Console.Write("*");
+                    if (gubbe[i, h] == null)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(gubbe[i, h]);
+                    }
                 }
-                else if (keyInfo.Key ==ConsoleKey.Backspace && pass.Length > 0)
-                {
-                    pass.RemoveAt(pass.Length - 1);
-                    Console.Write(" ⧵b ⧵b");
-                }
-            } while (keyInfo.Key != ConsoleKey.Enter);
-            {
-                return pass;
+                Console.WriteLine();
 
             }
         }
